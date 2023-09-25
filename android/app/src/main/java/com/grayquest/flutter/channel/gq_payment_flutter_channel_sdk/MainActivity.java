9b42476c932e41583e60c5341ae9f44db1e1975c
Avinash <avinash.soni@grayquest.com>
2023-09-25 18:31:15 +0530

    First Commit

---
+++ android/app/src/main/java/com/grayquest/flutter/channel/gq_payment_flutter_channel_sdk/MainActivity.java
@@ -0,0 +1,6 @@
+package com.grayquest.flutter.channel.gq_payment_flutter_channel_sdk;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
